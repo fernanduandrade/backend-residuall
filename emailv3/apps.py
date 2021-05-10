996a538415e2b41c03d922cfe8e2b90ee1d92a85
fernanduandrade <onanduandrade@gmail.com>
2021-05-10 17:57:42 -0300

    adicionar app para validar email usando EVA api

---
+++ emailv3/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Emailv3Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'emailv3'
--- eva/settings.py
+++ eva/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'index',
     'emailv1',
+    'emailv3',
     'rest_framework',
 ]
 
