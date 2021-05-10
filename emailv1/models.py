6397f9af2a96046af3276666717a036cf2a9d398
fernanduandrade <onanduandrade@gmail.com>
2021-05-10 18:09:01 -0300

    atualizar modelo para tabela Emailv1 e criar modelo para table Emailv3

--- emailv1/models.py
+++ emailv1/models.py
@@ -5,3 +5,4 @@ class Emailv1(models.Model):
     email_adress = models.CharField(max_length=100)
     domain = models.CharField(max_length=20)
     valid_syntax = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
--- emailv3/models.py
+++ emailv3/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Emailv3(models.Model):
+    email_address = models.CharField(max_length=100)
+    domain = models.CharField(max_length=20)
+    valid_syntax = models.BooleanField(default=False)
+    disposable = models.BooleanField(default=False)
+    webmail = models.BooleanField(default=False)
+    deliverable = models.BooleanField(default=False)
+    catch_all = models.BooleanField(default=False)
+    gibberish = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
