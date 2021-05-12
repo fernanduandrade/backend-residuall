21ff7ca3bd1f37f4d4119d5b7cdab81c70809245
fernanduandrade <onanduandrade@gmail.com>
2021-05-11 21:39:43 -0300

    implementar docker para rodar a aplicação e atualizar rotas

--- emailv1/urls.py
+++ emailv1/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('mail/validation/v1', views.validation_v1, name='validation_v1'),
+    path('mail/validation/v1/', views.validation_v1, name='validation_v1'),
     path('mail/v1', views.Emailv1List.as_view()),
 ]
